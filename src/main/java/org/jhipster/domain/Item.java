770b97e3bede4cf7bf40db1b1d487226c0d0be1a
Cedric <cedric.bulloni@gmail.com>
2017-09-22 15:59:50 +0200

    Initial commit

---
+++ src/main/java/org/jhipster/domain/Item.java
@@ -0,0 +1,117 @@
+package org.jhipster.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+/**
+ * A Item.
+ */
+@Entity
+@Table(name = "item")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Item implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @NotNull
+    @DecimalMin(value = "0")
+    @Column(name = "price", precision=10, scale=2, nullable = false)
+    private BigDecimal price;
+
+    @Column(name = "login")
+    private String login;
+
+    // jhipster-needle-entity-add-field - Jhipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public Item productId(Long productId) {
+        this.productId = productId;
+        return this;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Item price(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public Item login(String login) {
+        this.login = login;
+        return this;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    // jhipster-needle-entity-add-getters-setters - Jhipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        if (item.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), item.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+            "id=" + getId() +
+            ", productId='" + getProductId() + "'" +
+            ", price='" + getPrice() + "'" +
+            ", login='" + getLogin() + "'" +
+            "}";
+    }
+}
